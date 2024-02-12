3ab4ae22298f02040b62b714967c7aace87f2822
elliot b  =^._.^= ∫ <elliot@email.gd>
2024-02-12 16:10:48 -0500

    Updated 2 lines and deleted 2 in config.js

--- config.js
+++ config.js
@@ -4,12 +4,12 @@ const config = new Configstore('gpt-commit', {
     promptTemplates: {
         s: {
             maxTokens: 500,
-            diffCommand: 'git diff --compact-summary',
+            diffCommand: 'git diff --cached --compact-summary',
             prompt: 'Generate a succinct summary of the following code changes, with as much detail as possible, using no more than 50 characters.\n`',
         },
         l: {
             maxTokens: 4096,
-            diffCommand: 'git diff',
+            diffCommand: 'git diff --cached',
             prompt: 'Generate a summary of the code changes provided with as much detail as possible. Follow this format: commit should include title and body seperated by blank line. title should be imperative, start upper case, no periods. max 50 chars. body should explain what and why, word wrap at 72 chars. Shy away from describing each exact change, and lean more towards describing the whole of the changes in an understandable way, summing them up. Here is the code: \n`',
         },
     },
